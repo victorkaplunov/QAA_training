97c0dc82c74a4da49b2e922696d05749db3ee0a3
victorkaplunov <victorkaplunov@yandex.ru>
2019-06-19 10:29:18 +0300

    add english comments, and some PEPisation.

--- lesson001/test_lesson_01.py
+++ lesson001/test_lesson_01.py
@@ -6,12 +6,13 @@ import pytest
 import requests
 import re
 import csv
-"""Тест англоязычной JSON-версии web-сервиса ip-api.com, в тесте проводится проверка параметров по-умолчанию."""
+"""Test of the web service ip-api.com, the test checks the default parameters of the JSON version."""
+
 
 @pytest.fixture(scope='session')
 def result():
-    """Получение JSON с сетевыми параметрами текущего IP-адреса клиента.
-    Для измененеия ответа можно использовать прокси серверы из комментариев"""
+    """Get JSON with network parameters of the current client IP address.
+     To change the response, you can use proxy servers from the comments."""
     result = requests.get('http://ip-api.com/json',
                           # proxies={"http": "47.254.69.158:9999"}
                           # proxies={"http": "43.229.72.214:52360"}
@@ -23,7 +24,7 @@ def result():
 
 @pytest.fixture()
 def zone_list():
-    """Получаем список временных зон из CSV-файла"""
+    """Get a time zone list from the CSV file"""
     with open('zone.csv') as csvfile:
         read_csv = csv.reader(csvfile, delimiter=',')
         zone_list = [row[2] for row in read_csv]
@@ -32,7 +33,7 @@ def zone_list():
 
 @pytest.fixture()
 def country_list():
-    """Получаем список названий стран из CSV-файла"""
+    """Get a country list the CSV file"""
     with open('country.csv') as csvfile:
         read_csv = csv.reader(csvfile, delimiter=',')
         country_list = [row[1] for row in read_csv]
@@ -42,7 +43,7 @@ def country_list():
 
 @pytest.fixture()
 def country_code_list():
-    """Получаем список кодов стран из CSV-файла"""
+    """Get a country codes the CSV file."""
     with open('country.csv') as csvfile:
         read_csv = csv.reader(csvfile, delimiter=',')
         country_code_list = [row[0] for row in read_csv]
@@ -51,52 +52,69 @@ def country_code_list():
 
 
 def test_status_code(result, zone_list):
-    """Статус код HTTP запроса: 200"""
+    """Check the status of HTTP request."""
     assert result.status_code == 200
 
 
 def test_status(result):
-    """Значение поля status имеет значение success"""
-    assert (result.json()['status']) == 'success', "Значение поля status не равно строке 'success'"
+    """Check the 'status' field."""
+    assert (result.json()['status']) == 'success',\
+        "Значение поля status не равно строке 'success'"
 
 
 def test_value_type(result):
-    """Проверка типов данных для полей JSON"""
-    assert type(result.json()['as']) is str, "Содержимое поля 'as' не строка"
-    assert type(result.json()['city']) is str, "Содержимое поля 'city' не строка"
-    assert type(result.json()['isp']) is str, "Содержимое поля 'isp' не строка"
-    assert type(result.json()['org']) is str, "Содержимое поля 'org' не строка"
-    assert type(result.json()['region']) is str, "Содержимое поля 'region' не строка"
-    assert type(result.json()['regionName']) is str, "Содержимое поля 'regionName' не строка"
-    assert type(result.json()['zip']) is str, "Содержимое поля 'zip' не строка"
+    """
+    Check a data type of value for the this JSON fields:
+    'as', 'city', 'isp', 'org' 'region' 'regionName' """
+    assert type(result.json()['as']) is str,\
+        "The value of field 'as' not a string."
+    assert type(result.json()['city']) is str,\
+        "The value of field 'city' not a string."
+    assert type(result.json()['isp']) is str,\
+        "The value of field 'isp' not a string."
+    assert type(result.json()['org']) is str,\
+        "The value of field 'org' not a string."
+    assert type(result.json()['region']) is str,\
+        "The value of field 'region' not a string."
+    assert type(result.json()['regionName']) is str,\
+        "The value of field 'regionName' not a string."
+    assert type(result.json()['zip']) is str,\
+        "The value of field 'zip' not a string."
 
 
 def test_country_code_field(result, country_code_list):
-    """Значение поля countryCode входит в эталонный список кодов стран"""
-    assert (result.json()['countryCode']) in country_code_list, "Значение поля countryCode не входит в список кодов"
+    """Check that a value of 'countryCode' in the country code list."""
+    assert (result.json()['countryCode']) in country_code_list,\
+        "The value of field 'countryCode' not in the country code list."
 
 
 def test_country_field(result, country_list):
-    """Значение поля timezone входит в эталонный список временных зон"""
-    assert (result.json()['country']) in country_list, "Значение поля country не входит в список стран"
+    """Check that country list contain a value of 'countryCode' field."""
+    assert (result.json()['country']) in country_list,\
+        "The value of field 'countryCode' not in the country list."
 
 
 def test_timezone(result, zone_list):
-    """Значение поля timezone входит в эталонный список временных зон"""
-    assert (result.json()['timezone']) in zone_list, "Значение поля timezone не входит в список временных зон"
+    """Check that time zone list contain a value of 'timezone' field."""
+    assert (result.json()['timezone']) in zone_list,\
+        "The value of field 'timezone' not in the timezone list."
 
 
 def test_ip_adress(result):
-    """Значение поля query (IP-address) имеет допустимое значение"""
-    assert re.match(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
-                    result.json()['query']), "Содержимое поля IP-адреса не соответствует шаблону"
+    """Check format of a 'query' (IP-address) field/"""
+    assert re.match(r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)'
+                    r'{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$',
+                    result.json()['query']),\
+        "The value of a 'query' field is not correct IP address."
 
 
 def test_lat(result):
-    """Значение широты входит в допустимый диапазон"""
-    assert -90 <= result.json()['lat'] <= 90, "Значение поля lat не соответсвуюет диапазону допустимых значений"
+    """Check that a value of the 'lat' field is in a given range."""
+    assert -90 <= result.json()['lat'] <= 90,\
+        "Value of the 'lat' field is in a given range."
 
 
 def test_lon(result):
-    """Значение долготы входит в допустимый диапазон"""
-    assert -180 <= result.json()['lon'] <= 180, "Значение поля lon не соответсвуюет диапазону допустимых значений"
+    """Check that a value of the 'lon' field is in a given range."""
+    assert -180 <= result.json()['lon'] <= 180,\
+        "Value of the 'lon' field is in a given range."
