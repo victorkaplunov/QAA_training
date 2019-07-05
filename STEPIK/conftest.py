0ba9dce5b1f4e5be1386122f9fc3e0f2e4f77d34
victorkaplunov <victorkaplunov@yandex.ru>
2019-07-05 12:03:47 +0300

    Update conftest.py

--- STEPIK/conftest.py
+++ STEPIK/conftest.py
@@ -1,1 +1,29 @@
+"""Utilities for tests"""
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+def pytest_addoption(parser):
+    """
+    CLI parameter for test.
+    Парметр вызова теста из командной строки.
+    """
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose accept_languages HTTP request header value "
+                          "for browser. Accept any ISO-639 language codes.")
+
+
+@pytest.fixture()
+def browser(request):
+    """
+    The fixture for calling browser with given parameters.
+    Фикстура вызова браузера с заданными параметрами
+    """
+    user_language = request.config.getoption("language")
+    print(user_language)
+    options = Options()
+    options.add_experimental_option('prefs',
+                                    {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    return browser
