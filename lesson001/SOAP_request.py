8e285dd92a685e9c8ff9dbff6388ba513b7fdfac
victorkaplunov <victorkaplunov@yandex.ru>
2019-09-14 11:48:17 +0300

    add request to GetTimeTable service.

--- lesson001/SOAP_request.py
+++ lesson001/SOAP_request.py
@@ -4,3 +4,7 @@ import suds.client
 client = suds.client.Client('http://api-tt.belavia.by/TimeTable/Service.asmx?WSDL')
 result = client.service.GetAirportsList('EN')
 print(result)
+
+client = suds.client.Client('http://api-tt.belavia.by/TimeTable/Service.asmx?WSDL')
+result = client.service.GetTimeTable('SVO', 'Arrival', '2019-09-14')
+print(result)
